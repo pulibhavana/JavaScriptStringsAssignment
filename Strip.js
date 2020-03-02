4914e7ebcac8db512d8bfb570b1d4ced29c59f8b
pulibhavana <bhavana.puli@thoughtworks.com>
2020-03-02 12:51:23 +0530

    Modified

--- Strip.js
+++ Strip.js
@@ -1,44 +1,18 @@
 function main()
 {
-    string = " hello "
-    result = removeSpaces(string)
-    console.log(result)
+    console.log(removeSpaces('   hello    '))
 }
 
 function removeSpaces(string)
 {
-    len = string.length
-    var frontspace = countFrontSpaces(string)
-    var lastspace = countLastSpaces(string)
-    newstring = ''
-    for(var index = frontspace;index < len - lastspace;index++)
-        newstring += string[index]
-    return newstring
+   var frontspaces = string.split('').findIndex(isalphabet),newstr = '';
+   var lastspaces = string.split('').reverse().findIndex(isalphabet);
+   return string.slice(frontspaces,string.length - lastspaces);
 }
 
-function countFrontSpaces(string)
+function isalphabet(element,index)
 {
-    var frontspace = 0
-    for(var start = 0;start < string.length - 1;start ++)
-    {
-        if(string[start] != ' ')
-            break
-        frontspace = frontspace + 1
-    }
-    return frontspace
+    return element != ' ';
 }
-
-function countLastSpaces(string)
-{
-    var lastspace = 0
-    for(var end = string.length - 1;end > 0 ;end --)
-    {
-        if(string[end] != ' ')
-            break
-        lastspace = lastspace + 1
-    }
-    return lastspace
-}
-
 
 main()
