bc36a7065872bac780b88d15ad31f73facd2d772
pulibhavana <bhavana.puli@thoughtworks.com>
2020-03-02 11:52:53 +0530

    modified

--- CountVowels.js
+++ CountVowels.js
@@ -5,21 +5,15 @@ function main()
 
 function countVowels(string)
 {
-    let characters = string.split('');
+    var characters = string.split('');
     return characters.filter(isVowel).length;
 
 }
 
 function isVowel(character)
 {
-    vowels = ['a','e','i','o','u']
-    char = character.toLowerCase()
-    for(var index = 0;index < vowels.length;index++)
-    {
-        if(char == vowels[index])
-            return true
-    }
-    return false
+    var vowels = 'aeiou'
+    return vowels.includes(character);
 }
 
 main();
